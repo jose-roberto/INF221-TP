dd607aff128e2776b9a81804ce18b9d99a854328
jose-roberto <jose.junior5@ufv.br>
2024-08-06 20:08:57 -0300

    refactoring django workspace

--- web_application/gride_dashboard/apps.py
+++ web_application/gride_dashboard/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class GrideDashboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Gride_Dashboard'
+    name = 'gride_dashboard'
--- web_application/web_application/wsgi.py
+++ web_application/web_application/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Gride_Gride project.
+WSGI config for web_application project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Gride_Gride.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_application.settings')
 
 application = get_wsgi_application()
